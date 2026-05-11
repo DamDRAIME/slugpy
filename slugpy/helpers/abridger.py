6a0379b1bb77da025c08d636f3185562010bbe2a
Dam.Draime <dam.draime@gmail.com>
2026-05-11 19:59:21 -0400

    Ref.: Leverage `Script` class

--- slugpy/helpers/abridger.py
+++ slugpy/helpers/abridger.py
@@ -1,16 +1,17 @@
 from pathlib import Path
 
+from slugpy.dataset.dataset import Script
 from slugpy.helpers.utils import split_at_indentation
 
 
-def abridge(script_filepath: Path | str, based_on_annotations: bool = False) -> list[str]:
+def abridge(script_filepath: Path | str, based_on_labels: bool = False) -> list[str]:
     """
-    Abridges a screenplay by newlines between lines of the same nature, based on annotations if available, otherwise
+    Condenses a screenplay by newlines between lines of the same nature, based on annotations if available, otherwise
     (default) based on heuristics.
 
     Args:
         script_filepath (Path | str): Path to the input screenplay text file.
-        based_on_annotations (bool, optional): Whether to use annotations to line's nature. Defaults to False, in which
+        based_on_labels (bool, optional): Whether to use annotations to regroupe lines. Defaults to False, in which
             case heuristics are used such as indentation and case.
 
     Returns:
@@ -19,36 +20,28 @@ def abridge(script_filepath: Path | str, based_on_annotations: bool = False) -> 
     # TODO: Try to regroup in one line a dialogue block -> C (E?): U (P?) U
     abridged_script = []
     accumulated_line = []
-    prev_indent_tag = None
-    tags_flag = False
-    with open(script_filepath, "r", encoding="utf-8") as f:
-        line = f.readline()
-        parts = line.split("|", maxsplit=1)
-        if len(parts) > 1:
-            tags_flag = True
-            if not based_on_annotations:
-                print("Annotations have been detected and will be ignored as `based_on_annotations` is set to False.")
-        if based_on_annotations and not tags_flag:
-            raise ValueError("No tags detected in script!")
-        f.seek(0)
-        for line in f:
-            if tags_flag:
-                tags, line = line.split("|", maxsplit=1)
-                tag = tags.split(",", maxsplit=1)[0]
-            line = line.rstrip()
-            line, indent = split_at_indentation(line)
-            if not line:
-                continue
-            if based_on_annotations and "O" in tags:
-                continue
+    prev_indent_or_label = None
+    for slp in Script(script_filepath, 1, random_start=False, iter_as_dict=False):
+        if slp.line.labels and not based_on_labels:
+            print("Annotations have been detected and will be ignored as `based_on_annotations` is set to False.")
+        elif based_on_labels and not slp.line.labels:
+            raise ValueError("No annotations detected in script!")
 
-            key = tag if based_on_annotations else indent
-            if prev_indent_tag is not None and key != prev_indent_tag:
-                abridged_script.append(" ".join(accumulated_line))
-                accumulated_line = []
+        line = slp.line.line.rstrip()
+        line, indent = split_at_indentation(line)
+        if not line:
+            continue
+        label = slp.line.primary_label
+        if based_on_labels and label == "O":
+            continue
 
-            accumulated_line.append(line)
-            prev_indent_tag = key
-        if accumulated_line:
+        indent_or_label = label if based_on_labels else indent
+        if prev_indent_or_label is not None and indent_or_label != prev_indent_or_label:
             abridged_script.append(" ".join(accumulated_line))
+            accumulated_line = []
+
+        accumulated_line.append(line)
+        prev_indent_or_label = indent_or_label
+    if accumulated_line:
+        abridged_script.append(" ".join(accumulated_line))
     return abridged_script
