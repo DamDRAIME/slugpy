Fix.: Fix output masking via `line_span_mask` and dimensions

src/slugpy/model/lstm_based.py
@@ -2,6 +2,7 @@ from typing import Any
 
 import torch
 from torch import nn
+from transformers import AutoModel
 
 from slugpy.dataset.label import N_LABELS
 from slugpy.model.features_extractor import ScriptLineFeaturesExtractor
@@ -9,28 +10,44 @@ from slugpy.model.tokenizer import TokenizerWithCtx
 
 
 class LSTMClassifier(nn.Module):
-    def __init__(self, tokenizer_model_name: str, features_extractor: ScriptLineFeaturesExtractor, bidirectional: bool):
+    def __init__(
+        self,
+        model_name: str,
+        features_extractor: ScriptLineFeaturesExtractor,
+        bidirectional: bool = True,
+        **tokenizer_kwargs,
+    ):
         super().__init__()
-        self.tokenizer = TokenizerWithCtx(model_name=tokenizer_model_name)  # "sentence-transformers/all-mpnet-base-v2"
+        self.tokenizer = TokenizerWithCtx(model_name, **tokenizer_kwargs)  # "sentence-transformers/all-mpnet-base-v2"
+        self.model = AutoModel.from_pretrained(model_name)
+        self.model.resize_token_embeddings(len(self.tokenizer))
         self.features_extractor = features_extractor
-        self.features_size = self.tokenizer.dim + self.features_extractor.n_features
         self.hidden_size = 256
         self.n_labels = N_LABELS
         self.bidirectional = bidirectional
-        self.lstm = nn.LSTM(self.features_size, self.hidden_size, batch_first=True, bidirectional=bidirectional)
-        self.classifier = nn.Linear((1 + int(self.bidirectional)) * self.hidden_size, self.n_labels)
+        self.lstm = nn.LSTM(
+            self.model.config.hidden_size, self.hidden_size, batch_first=True, bidirectional=self.bidirectional
+        )
+        self.classifier = nn.Linear(
+            ((1 + int(self.bidirectional)) * self.hidden_size) + self.features_extractor.n_features, self.n_labels
+        )
 
     def forward(self, batch: dict[str, Any]) -> torch.LongTensor:
-        lines_with_ctx_tokenized = self.tokenizer(
-            batch["line_with_ctx"], return_line_span_mask=True, return_attention_mask=False
-        )  # (B, 2*Ctx + 1, ~) -> (B, T)
-        lines_features = self.features_extractor(batch["line"])  # (B, n_features)
-        input = torch.cat([lines_with_ctx_tokenized["input_ids"], lines_features], dim=1)  # (B, T + n_features)
-        last_hidden, _ = self.lstm(input)  # (B, T + n_features, H * (1 + bidirectional))
+        # Tokenizer
+        lines_with_ctx_tokenized = self.tokenizer(batch["line_with_ctx"])  # (B, 2*Ctx + 1, ~) -> (B, T)
+        input_ids = lines_with_ctx_tokenized["input_ids"]
+        attention_mask = lines_with_ctx_tokenized["attention_mask"]
+        # Embedding
+        lines_embeddings = self.model(input_ids=input_ids, attention_mask=attention_mask).last_hidden_state  # (B, T, H)
+        # LSTM
+        last_hidden, _ = self.lstm(lines_embeddings)  # (B, T, H * (1 + bidirectional))
         # Mean-pool the target line span
-        line_span_mask = lines_with_ctx_tokenized["line_span_mask"]
-        tokens_pooled = last_hidden[:, : self.tokenizer.dim][line_span_mask].mean(dim=1)  # (B, H* (1 + bidirectional))
-        features_pooled = last_hidden[:, self.tokenizer.dim :].mean(dim=1)  # (B, H* (1 + bidirectional))
-        pooled = torch.cat([tokens_pooled, features_pooled], dim=1)  # (B, H * (1 + bidirectional) * 2)
-        logits = self.classifier(pooled)
-        return logits.argmax(dim=1)
+        line_span_mask = torch.broadcast_to(lines_with_ctx_tokenized["line_span_mask"].unsqueeze(-1), last_hidden.shape)
+        line_span_mask = line_span_mask.to(torch.bool)
+        masked_hidden = last_hidden.masked_fill(~line_span_mask, float("nan"))
+        pooled = torch.nanmean(masked_hidden, 1)  # (B, H * (1 + bidirectional))
+        # Concat
+        lines_features, _headers = self.features_extractor(batch["line"])  # (B, n_features)
+        concat = torch.cat([pooled, lines_features], dim=1)  # (B, (H * (1 + bidirectional)) + n_features)
+        logits = self.classifier(concat)  # (B, n_labels)
+        return logits

src/slugpy/model/transformer_based.py
@@ -11,7 +11,7 @@ from slugpy.model.tokenizer import TokenizerWithCtx
 class TransformerClassifier(nn.Module):
     def __init__(self, model_name: str, **tokenizer_kwargs):
         super().__init__()
-        self.tokenizer = TokenizerWithCtx(model_name, **tokenizer_kwargs)
+        self.tokenizer = TokenizerWithCtx(model_name, **tokenizer_kwargs)  # "microsoft/deberta-v3-base"
         self.model = AutoModel.from_pretrained(model_name)
         self.model.resize_token_embeddings(len(self.tokenizer))
         self.n_labels = N_LABELS
@@ -19,12 +19,18 @@ class TransformerClassifier(nn.Module):
         self.classifier = nn.Linear(hidden, self.n_labels)
 
     def forward(self, batch: dict[str, Any]) -> torch.LongTensor:
+        # Tokenizer
         lines_with_ctx_tokenized = self.tokenizer(batch["line_with_ctx"])  # (B, 2*Ctx + 1, ~) -> (B, T)
         input_ids = lines_with_ctx_tokenized["input_ids"]
         attention_mask = lines_with_ctx_tokenized["attention_mask"]
+        # Transformer
         outputs = self.model(input_ids=input_ids, attention_mask=attention_mask)
         last_hidden = outputs.last_hidden_state  # (B, T, H)
         # Mean-pool the target line span
-        pooled = last_hidden[lines_with_ctx_tokenized["line_span_mask"]].mean(dim=1)  # (B, H)
-        logits = self.classifier(pooled)  # (B, n_labels)
+        line_span_mask = torch.broadcast_to(lines_with_ctx_tokenized["line_span_mask"].unsqueeze(-1), last_hidden.shape)
+        line_span_mask = line_span_mask.to(torch.bool)
+        masked_hidden = last_hidden.masked_fill(~line_span_mask, float("nan"))
+        pooled = torch.nanmean(masked_hidden, 1)  # (B, H)
+        # Classifier
+        logits = self.classifier(pooled.to(torch.float32))  # (B, n_labels)
         return logits